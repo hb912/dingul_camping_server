19e7b0af65728a9f90b54b8a9427518eff8fc2f6
hb912 <43055448+hb912@users.noreply.github.com>
2022-07-15 11:46:17 +0900

    Feat: 회원가입 userService 작성
    회원가입 관련 userService 작성
    입력된 비밀번호는 bcrypt로 해쉬하여 db에 입력한다.

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -8,6 +8,23 @@ class UserService {
   }
 
   // 회원가입
+  async addUser(userInfo) {
+    const { email, name, password } = userInfo;
+
+    // 이메일 중복 확인
+    const user = await this.userModel.findByEmail(email);
+    if (user) {
+      throw new Error(
+        '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.'
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUserInfo = { name, email, password: hashedPassword };
+    const createdNewUser = await this.userModel.create(newUserInfo);
+    return createdNewUser;
+  }
+
   // 로그인
   async getUserToken(user) {
     // 로그인 성공 -> JWT 웹 토큰 생성
