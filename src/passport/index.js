77240bffa568b67a9c9a20f0f31c4f3c0b35fd8b
hb912 <43055448+hb912@users.noreply.github.com>
2022-07-16 01:56:42 +0900

    Fix: session을 사용하기 위해 미들웨어를 추가합니다
    passport 관련 serialize, deserialize 코드를 추가합니다.
    유저 db에 oauth provider를 기록하기 위해 스키마를 수정합니다.

--- src/passport/index.js
+++ src/passport/index.js
@@ -3,6 +3,16 @@ import { local, jwt, kakao } from './strategies';
 
 export default () => {
   // local strategy 사용
+  passport.serializeUser((user, done) => {
+    console.log(user);
+    done(null, user);
+  });
+
+  passport.deserializeUser((obj, done) => {
+    console.log(obj);
+    done(null, obj);
+  });
+
   passport.use(local);
   passport.use(jwt);
   passport.use(kakao);
