de90edac5802970195dcbb934dbc3fe9cd25413d
hb912 <43055448+hb912@users.noreply.github.com>
2022-07-22 21:10:19 +0900

    Feat: 비밀번호 찾기, 유저정보 수정 api 작성
    비밀번호 찾기 -> 레디스로 keyvalue 가진 후 해당키를 param으로 붙여서 메일로 링크를 보내주면 이링크로 유저 접속시 레디스 키를 조회하여 비밀번호 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,22 @@
 import { Router } from 'express';
 import { userService } from '../services';
+import { userService, mailer } from '../services';
 import passport from 'passport';
 import { loginRequired } from '../middleware/loginRequired';
 import bcrypt from 'bcrypt';
+import * as redis from 'redis';
 
 const userRouter = Router();
+
+const redisClient = redis.createClient({ url: process.env.REDIS_URL });
+
+redisClient.connect();
+redisClient.on('error', (err) => {
+  console.log(err);
+});
+redisClient.on('ready', () => {
+  console.log('정상적으로 Redis 서버에 연결되었습니다.');
+});
 
 userRouter.post('/register', async (req, res, next) => {
   try {
@@ -101,6 +113,70 @@ userRouter.get('/logout', loginRequired, async (req, res) => {
   req.session.save(function () {
     res.status(200).json({ message: 'Ok' });
   });
+});
+
+userRouter.post('/findPW', async (req, res, next) => {
+  const { email } = req.body;
+  const number = Math.random().toString(18).slice(2);
+  try {
+    const userEmail = await userService.getUserByEmail(email);
+    const saveKey = await redisClient.setEx(number, 180, userEmail);
+    console.log(saveKey);
+    if (!saveKey) {
+      res.status(400).json({ error: 'redis 저장에 실패했습니다.' });
+    }
+    const result = await mailer(email, number);
+    res.status(200).json(result);
+  } catch (e) {
+    next(e);
+  }
+});
+
+userRouter.get('/findPW/:redisKey', async (req, res, next) => {
+  const { redisKey } = req.params;
+  try {
+    const userID = await redisClient.get(redisKey);
+    if (!userID) {
+      res.status(400).json({ error: '유효기간이 지났거나 잘못된 링크입니다.' });
+    }
+    res.status(200).json(userID);
+  } catch (e) {
+    next(e);
+  }
+});
+
+userRouter.get('/findEmail', async (req, res, next) => {
+  const { name } = req.query;
+  if (!name) {
+    res.status(400).json('이름을 입력해 주세요.');
+  }
+  try {
+    const user = await userService.getUserByName(name);
+    if (!user) {
+      res.status(400).json({ error: '해당 유저가 없습니다.' });
+    }
+    const { email } = user;
+    res.status(200).json(email);
+  } catch (e) {
+    next(e);
+  }
+});
+
+userRouter.patch('/user', loginRequired, async (req, res, next) => {
+  try {
+    const { name, email, password, phoneNumber } = req.body;
+    const userID = req.currentUserId;
+    const result = await userService.update({
+      name,
+      email,
+      password,
+      phoneNumber,
+      userID,
+    });
+    res.status(200).json(result);
+  } catch (e) {
+    next(e);
+  }
 });
 
 userRouter.delete('/user', loginRequired, async (req, res, next) => {
