31850a400c335c4ab450c4c20d96fb661f4b4c34
hb912 <43055448+hb912@users.noreply.github.com>
2022-07-26 10:56:24 +0900

    Fis: 리뷰 모델 수정

--- src/db/models/userModel.js
+++ src/db/models/userModel.js
@@ -40,16 +40,16 @@ export class UserModel {
     return users;
   }
 
-  async update({ userId, update }) {
+  async update({ userID, update }) {
     //유저 정보 수정(아마도 비밀번호 변경 혹은 주소 변경)
-    const filter = { _id: userId };
+    const filter = { _id: userID };
     const option = { returnOriginal: false };
 
     const updatedUser = await User.findOneAndUpdate(filter, update, option);
     return updatedUser;
   }
 
-  async delete(userId) {
+  async deleteUser(userId) {
     const deleteUser = await User.deleteOne({ _id: userId });
     return deleteUser;
   }
