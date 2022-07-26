ee3cc1cbceb8c624a19a3510d9e1a7c5713bd5ad
jhoon9494 <wjdgns9406@gmail.com>
2022-07-26 21:11:39 +0900

    Fix: CORS 관련 코드 수정

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ import cookieParser from 'cookie-parser';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
